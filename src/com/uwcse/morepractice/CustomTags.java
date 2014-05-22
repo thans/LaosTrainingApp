139285d84320c89c37465269b1c65454af63864e
thans <toreh@Tores-MacBook-Air.local>
2014-05-21 22:47:02 -0700

    Adding to history state based on script from unicef
    http://www.who.int/immunization_standards/vaccine_quality/introduction_g
    uidelines_fridgetag.pdf

--- src/com/uwcse/morepractice/CustomTags.java
+++ src/com/uwcse/morepractice/CustomTags.java
@@ -15,6 +15,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -25,8 +27,19 @@ import android.os.Build;
 public class CustomTags extends Activity {
 	private static Random r = new Random();
 	private static int stepNumber;
-	private static final int NUM_STEPS = 7;
 	private static String[] stepInstructions;
+	
+	private static final int HIGH_ALARM_MARGIN_TOP = 107;
+	private static final int LOW_ALARM_MARGIN_TOP = 120;
+	private static final int TODAY_ALARM_LEFT = 790;
+	private static final int DAILY_WIDTH = 23;
+	
+	private static final String[] TEMPS = {"06.1",  "04.7",  "09.7",  "03.7",  "06.4",  "-02.3", "09.6",  "03.5"};
+	private static final String[] TIMES = {"00:00", "00:00", "04:41", "00:00", "00:00", "03:12", "12:52", "00:00"};
+	
+	private static final int LOW_ALARM_INDEX = 5;
+	private static final int HIGH_ALARM_INDEX = 6;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,7 +47,7 @@ public class CustomTags extends Activity {
 
 //		TextView timeText = (TextView) this.findViewById(R.id.time_text);
 		Button set = (Button) this.findViewById(R.id.set_button);
-		Button read = (Button) this.findViewById(R.id.read_button);
+		
 		
 		final CustomTags act = this;
         set.setOnClickListener(new View.OnClickListener() {
@@ -42,12 +55,7 @@ public class CustomTags extends Activity {
         		act.setClick();
             }
         });
-        
-        read.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-        		act.readClick();
-            }
-        });
+
         
         ImageButton next = (ImageButton) this.findViewById(R.id.next_instruction);
         
@@ -63,15 +71,190 @@ public class CustomTags extends Activity {
 
 	protected void next() {
 		stepNumber++;
-		if (stepNumber >= NUM_STEPS) {
-			this.finish();
+		if (stepNumber >= stepInstructions.length) {
+			Button read = (Button) this.findViewById(R.id.read_button);
+			final CustomTags act = this;
+	        read.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View v) {
+	        		act.readClick();
+	            }
+	        });
+	        
+			TextView instructions = (TextView) this.findViewById(R.id.custom_tags_instructions);
+			instructions.setText("HISTORY MODE\nClick READ to enter history mode");
+			ImageButton next = (ImageButton) this.findViewById(R.id.next_instruction);
+			if (next != null) {
+				RelativeLayout nextLayout = (RelativeLayout) this.findViewById(R.id.next_button_layout);
+				nextLayout.setVisibility(RelativeLayout.GONE);
+			}
+			return;
 		}
+		System.out.println("Step number: " + stepNumber);
 		
 		TextView instructions = (TextView) this.findViewById(R.id.custom_tags_instructions);
 		instructions.setText(stepInstructions[stepNumber]);
 	}
 
+	private void goToHistoryMode() {
+		RelativeLayout relativeLayout = (RelativeLayout) this.findViewById(R.id.custom_tags_relative_layout);
+		
+		
+		TextView instructions = (TextView) this.findViewById(R.id.custom_tags_instructions);
+		instructions.setText("HISTORY MODE");
+		
+		final TextView highAlarm = new TextView(this);//(TextView) this.findViewById(R.id.alarm_up);
+		highAlarm.setText(getResources().getString(R.string.up_arrow));
+		highAlarm.setTextSize(24);
+		RelativeLayout.LayoutParams layout = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+		layout.addRule(RelativeLayout.ALIGN_LEFT, R.id.fridge_tag);
+		layout.addRule(RelativeLayout.ALIGN_TOP, R.id.fridge_tag);
+		layout.leftMargin = TODAY_ALARM_LEFT;
+		layout.topMargin = HIGH_ALARM_MARGIN_TOP;
+		relativeLayout.addView(highAlarm, layout);
+		
+		final Animation anim = new AlphaAnimation(0.0f, 1.0f);
+		anim.setDuration(400); //You can manage the time of the blink with this parameter
+		anim.setStartOffset(20);
+		anim.setRepeatMode(Animation.REVERSE);
+		anim.setRepeatCount(Animation.INFINITE);
+		highAlarm.startAnimation(anim);
+		stepNumber = 0;
+		TextView duration = (TextView) this.findViewById(R.id.time_text);
+		duration.setText(TIMES[stepNumber]);
+		
+		TextView temp = (TextView) this.findViewById(R.id.temp_text);
+		temp.setText(TEMPS[stepNumber]);
+		
+//		TextView ok = (TextView) this.findViewById(R.id.alarm_ok_text);
+//		ok.setVisibility(TextView.INVISIBLE);
+		
+//		TextView alarm = (TextView) this.findViewById(R.id.alarm_text);
+//		alarm.setVisibility(TextView.VISIBLE);
+//		alarm.setText("ALARM");
+		
+		
+		stepInstructions = getResources().getStringArray(R.array.custom_tags_history_instructions);
+		
+        Button read = (Button) this.findViewById(R.id.read_button);
+        final CustomTags act = this;
+        
+        read.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+        		act.nextHistory(anim, highAlarm);
+            }
+        });
+        
+		// reenable the next button
+		/*
+		RelativeLayout nextLayout = (RelativeLayout) this.findViewById(R.id.next_button_layout);
+		nextLayout.setVisibility(RelativeLayout.VISIBLE);
+		
+		stepNumber = -1;
+		stepInstructions = getResources().getStringArray(R.array.custom_tags_history_instructions);
+		
+        ImageButton next = (ImageButton) this.findViewById(R.id.next_instruction);
+        final CustomTags act = this;
+        
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+        		act.nextHistory(anim, highAlarm);
+            }
+        });
+        */
+	}
+
+	protected void nextHistory(Animation anim, final TextView alarm) {
+		anim.cancel();
+
+		stepNumber++;
+		if (stepNumber >= TEMPS.length) {
+			finish();
+			return;
+		}
+		
+		TextView lowAlarm = (TextView) this.findViewById(R.id.alarm_down);
+		
+		if (stepNumber == LOW_ALARM_INDEX) {
+			lowAlarm.setVisibility(TextView.INVISIBLE);
+		} else {
+			lowAlarm.setVisibility(TextView.VISIBLE);
+		}
+		
+		TextView highAlarm = (TextView) this.findViewById(R.id.alarm_up);
+		if (stepNumber == HIGH_ALARM_INDEX) {
+			highAlarm.setVisibility(TextView.INVISIBLE);
+		} else {
+			highAlarm.setVisibility(TextView.VISIBLE);
+		}
+		
+		RelativeLayout.LayoutParams layout = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+		layout.addRule(RelativeLayout.ALIGN_LEFT, R.id.fridge_tag);
+		layout.addRule(RelativeLayout.ALIGN_TOP, R.id.fridge_tag);
+		layout.leftMargin = TODAY_ALARM_LEFT - DAILY_WIDTH * ((stepNumber / 2));
+		
+		// Set the temp
+		TextView temp = (TextView) this.findViewById(R.id.temp_text);
+		temp.setText(TEMPS[stepNumber]);
+		
+		// Set the time
+		TextView time = (TextView) this.findViewById(R.id.time_text);
+		time.setText(TIMES[stepNumber]);
+		
+		// Set the alarm status
+		TextView okayStatus = (TextView) this.findViewById(R.id.alarm_ok_text);
+		TextView alarmStatus = (TextView) this.findViewById(R.id.alarm_text);
+		
+		if (alarm()) {
+			alarmStatus.setVisibility(TextView.VISIBLE);
+			okayStatus.setVisibility(TextView.INVISIBLE);
+		} else {
+			okayStatus.setVisibility(TextView.VISIBLE);
+			alarmStatus.setVisibility(TextView.INVISIBLE);
+		}
+		
+		
+		
+		if (stepNumber % 2 == 1) { // Low alarms
+			alarm.setText(getResources().getString(R.string.down_arrow));
+			layout.topMargin = LOW_ALARM_MARGIN_TOP;
+		} else {
+			alarm.setText(getResources().getString(R.string.up_arrow));
+			layout.topMargin = HIGH_ALARM_MARGIN_TOP;
+		}
+		alarm.setLayoutParams(layout);
+		
+		final Animation animate = new AlphaAnimation(0.0f, 1.0f);
+		animate.setDuration(400); //You can manage the time of the blink with this parameter
+		animate.setStartOffset(20);
+		animate.setRepeatMode(Animation.REVERSE);
+		animate.setRepeatCount(Animation.INFINITE);
+		alarm.startAnimation(animate);
+		/*
+        Button read = (Button) this.findViewById(R.id.read_button);
+        final CustomTags act = this;
+        
+        read.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+        		act.nextHistory(animate, alarm);
+            }
+        });
+        */
+/*
+		TextView instructions = (TextView) this.findViewById(R.id.custom_tags_instructions);
+		instructions.setText(stepInstructions[stepNumber]);
+		*/
+	}
+
+	private boolean alarm() {
+		String time = TIMES[stepNumber];
+		double temp = Double.parseDouble(TEMPS[stepNumber]);
+		int hour = Integer.parseInt(time.split(":")[0]);
+		return ((temp >= 8 && hour >= 10) || (temp < -.05 && hour >= 1));
+	}
+
 	protected void readClick() {
+		goToHistoryMode();
+		/*
 		TextView tempText = (TextView) this.findViewById(R.id.temp_text);
 		TextView alarmText = (TextView) this.findViewById(R.id.alarm_text);
 		
@@ -81,7 +264,7 @@ public class CustomTags extends Activity {
 			alarmText.setText("ALM");
 		} else {
 			alarmText.setText("OK");
-		}
+		} */
 	}
 
 	protected void setClick() {
