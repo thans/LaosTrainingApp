7e5b88246eb35c186f87f778d86c8a018af2c2a2
samb0303 <samb0303@hotmail.com>
2014-05-25 21:32:29 -0700

    Texting semi working

--- src/com/uwcse/morepractice/SMSActivity.java
+++ src/com/uwcse/morepractice/SMSActivity.java
@@ -29,13 +29,316 @@ public class SMSActivity extends Activity {
 	private static int stepNumber;
 	private static String[] stepInstructions;
 	
+	private int[] counters;
+	private char[] text;
+	private int last;
+	private int screenCount;
+	private int letterCount;
+	boolean firstClick = true;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sms);
 		
-        
+		
+		
+		RelativeLayout layout = (RelativeLayout) this.findViewById(R.id.sms_relative_layout);
+		
+		counters = new int[10];
+		text = new char[48];
+		screenCount = 0;
+		letterCount = 0;
+		last = 0;
+		
+		final Button back = (Button) findViewById(R.id.back_button);
+		final Button one = (Button) findViewById(R.id.one_button);
+		final Button two = (Button) findViewById(R.id.two_button);
+		final Button three = (Button) findViewById(R.id.three_button);
+		final Button four = (Button) findViewById(R.id.four_button);
+		final Button five = (Button) findViewById(R.id.five_button);
+		final Button six = (Button) findViewById(R.id.six_button);
+		final Button seven = (Button) findViewById(R.id.seven_button);
+		final Button eight = (Button) findViewById(R.id.eight_button);
+		final Button nine = (Button) findViewById(R.id.nine_button);
+		final Button zero = (Button) findViewById(R.id.zero_button);
+		final Button next = (Button) findViewById(R.id.next_button);
+		
+		back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	letterCount = 0;
+            	last = 0;
+            	TextView screen = (TextView) findViewById(R.id.phone_screen);
+            	
+            	if(screenCount != 0){
+                	screenCount--;
+                	screen.setText(text, 0, screenCount +1);
+                } else {
+                	screen.setText(text, 0, screenCount);
+                	firstClick = true;
+                }
+            }
+        });
+		
+		next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	letterCount = 0;
+            	last = 0;
+            	TextView screen = (TextView) findViewById(R.id.phone_screen);
+            	
+            	screenCount++;
+            }
+        });
+		
+		one.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (screenCount < text.length - 1) {
+	            	if (firstClick) {
+	            		firstClick = false;
+	            		last = one.getId();
+	            	} 
+	            	screenCount++;
+	            	TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c = 49;
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+	                
+            	}
+            }
+        });
+		
+		two.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (screenCount < text.length - 1) {
+	            	if (firstClick) {
+	            		firstClick = false;
+	            		last = two.getId();
+	            	} else if (two.getId() != last) {
+	            		last = two.getId();
+	            		screenCount++;
+	            		letterCount = 0;
+	            	}
+	            	TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c;
+	            	if(letterCount == 3){
+	                	c = 50;
+	                	letterCount = 0;
+	                } else {
+	                	c = 'A' + letterCount;
+	                	letterCount++;
+	                }
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+            	}
+            }
+        });
+		
+		three.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (screenCount < text.length - 1) {
+	                if (firstClick) {
+	            		firstClick = false;
+	            		last = three.getId();
+	            	} else if (three.getId() != last) {
+	            		last = three.getId();
+	            		screenCount++;
+	            		letterCount = 0;
+	            	}
+	                TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c;
+	            	if(letterCount == 3){
+	                	c = 51;
+	                	letterCount = 0;
+	                } else {
+	                	c = 'D' + letterCount;
+	                	letterCount++;
+	                }
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+
+                }
+            }
+        });
+		
+		four.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (screenCount < text.length - 1) {
+	                if (firstClick) {
+	            		firstClick = false;
+	            		last = four.getId();
+	            	} else if (four.getId() != last) {
+	            		last = four.getId();
+	            		screenCount++;
+	            		letterCount = 0;
+	            	}
+	                TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c;
+	            	if(letterCount == 3){
+	                	c = 52;
+	                	letterCount = 0;
+	                } else {
+	                	c = 'G' + letterCount;
+	                	letterCount++;
+	                }
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+
+                }
+            }
+        });
+		
+		five.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (screenCount < text.length - 1) {
+	                if (firstClick) {
+	            		firstClick = false;
+	            		last = five.getId();
+	            	} else if (five.getId() != last) {
+	            		last = five.getId();
+	            		screenCount++;
+	            		letterCount = 0;
+	            	}
+	                TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c;
+	            	if(letterCount == 3){
+	                	c = 53;
+	                	letterCount = 0;
+	                } else {
+	                	c = 'J' + letterCount;
+	                	letterCount++;
+	                }
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+
+                }
+            }
+        });
+		
+		six.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (screenCount < text.length - 1) {
+	                if (firstClick) {
+	            		firstClick = false;
+	            		last = six.getId();
+	            	} else if (six.getId() != last) {
+	            		last = six.getId();
+	            		screenCount++;
+	            		letterCount = 0;
+	            	}
+	                TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c;
+	            	if(letterCount == 3){
+	                	c = 54;
+	                	letterCount = 0;
+	                } else {
+	                	c = 'M' + letterCount;
+	                	letterCount++;
+	                }
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+
+                }
+            }
+        });
+		
+		seven.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (screenCount < text.length - 1) {
+	                if (firstClick) {
+	            		firstClick = false;
+	            		last = seven.getId();
+	            	} else if (seven.getId() != last) {
+	            		last = seven.getId();
+	            		screenCount++;
+	            		letterCount = 0;
+	            	}
+	                TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c;
+	            	if(letterCount == 4){
+	                	c = 55;
+	                	letterCount = 0;
+	                } else {
+	                	c = 'P' + letterCount;
+	                	letterCount++;
+	                }
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+
+                }
+            }
+        });
+		
+		
+		eight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (screenCount < text.length - 1) {
+	                if (firstClick) {
+	            		firstClick = false;
+	            		last = eight.getId();
+	            	} else if (eight.getId() != last) {
+	            		last = eight.getId();
+	            		screenCount++;
+	            		letterCount = 0;
+	            	}
+	                TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c;
+	            	if(letterCount == 3){
+	                	c = 56;
+	                	letterCount = 0;
+	                } else {
+	                	c = 'T' + letterCount;
+	                	letterCount++;
+	                }
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+
+                }
+            }
+        });
+		
+		nine.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (screenCount < text.length - 1) {
+	                if (firstClick) {
+	            		firstClick = false;
+	            		last = nine.getId();
+	            	} else if (nine.getId() != last) {
+	            		last = nine.getId();
+	            		screenCount++;
+	            		letterCount = 0;
+	            	}
+	                TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c;
+	            	if(letterCount == 4){
+	                	c = 57;
+	                	letterCount = 0;
+	                } else {
+	                	c = 'W' + letterCount;
+	                	letterCount++;
+	                }
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+
+                }
+            }
+        });
+		
+		
+		zero.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	if (screenCount < text.length - 1) {
+	            	if (firstClick) {
+	            		firstClick = false;
+	            		last = zero.getId();
+	            	} 
+	            	screenCount++;
+	            	TextView screen = (TextView) findViewById(R.id.phone_screen);
+	                int c = 48;
+	                text[screenCount] = (char) c;
+	                screen.setText(text, 0, screenCount + 1);
+            	}
+            }
+        });
 	}
 
 
